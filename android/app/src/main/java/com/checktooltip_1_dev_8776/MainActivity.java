ebfc1e4dca6e095dcfac5f91b1c9abfd0da968e2
Crowdbotics <team@crowdbotics.com>
2020-08-11 04:59:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/checktooltip_1_dev_8776/MainActivity.java
@@ -0,0 +1,15 @@
+package com.checktooltip_1_dev_8776;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "checktooltip_1_dev_8776";
+    }
+}
